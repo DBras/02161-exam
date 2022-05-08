7bfd1be3cb03f914d982acd4c93a1a8261e578b5
DBras <danielbrasholt@gmail.com>
2022-05-08 16:10:03 +0200

    Add UI functionality for assigning managers and creating developers

--- src/main/java/plannerapp/UserInterface.java
+++ src/main/java/plannerapp/UserInterface.java
@@ -7,12 +7,13 @@ import java.util.*;
 public class UserInterface {
     PlannerApp planner_app;
     Scanner console;
-    private String welcome_message_ascii = " ____  _                              _\n" +
+    private final String welcome_message_ascii = " ____  _                              _\n" +
             "|  _ \\| | __ _ _ __  _ __   ___ _ __ / \\   _ __  _ __\n" +
             "| |_) | |/ _` | '_ \\| '_ \\ / _ \\ '__/ _ \\ | '_ \\| '_ \\\n" +
             "|  __/| | (_| | | | | | | |  __/ | / ___ \\| |_) | |_) |\n" +
             "|_|   |_|\\__,_|_| |_|_| |_|\\___|_|/_/   \\_\\ .__/| .__/\n" +
             "                                          |_|   |_|\n";
+    private final String syntax_error_message = "Command syntax incorrect. See help message for correct usage";
 
     public UserInterface(PlannerApp planner_app) {
         this.planner_app = planner_app;
@@ -37,7 +38,9 @@ public class UserInterface {
         System.out.println("The available commands are as follows:\n" +
                 "project_add <title> <start date> (e.g. project_add New Project 2022-02-23) - add new project\n" +
                 "project_list - list all projects in the system\n" +
-                "search_id <ID> (e.g. search 22001) - display information for project with <ID>\n" +
+                "search_id <ID> (e.g. search_id 22001) - display information for project with <ID>\n" +
+                "developer_add <initials> (e.g. developer_add Huba) - add new developer to the system\n" +
+                "assign_manager <project_id> <manager_initials> (e.g. assign_manager 22001 Huba) - assign project manager\n" +
                 "quit or exit - exit the program\n" +
                 "help or ? - display this help message\n");
     }
@@ -83,14 +86,62 @@ public class UserInterface {
 
     public void searchProject(String[] command_body) {
         if (command_body.length != 2) {
-            System.out.println("Command syntax incorrect. See help message for correct usage");
-        }
-        Project matching = this.planner_app.searchProjectsById(Integer.parseInt(command_body[1]));
-        if (matching == null) {
-            System.out.printf("No projects with id %s found%n", command_body[1]);
+            System.out.println(syntax_error_message);
         } else {
-            System.out.printf("ID: %s, title: %s, scheduled starting time: %s%n",
-                    matching.getProject_id(), matching.getTitle(), matching.getStart_date());
+            Project matching = this.planner_app.searchProjectsById(Integer.parseInt(command_body[1]));
+            if (matching == null) {
+                System.out.printf("No projects with id %s found%n", command_body[1]);
+            } else {
+                Developer manager = matching.getProjectManager();
+                String manager_string;
+                if (manager == null) {
+                    manager_string = "None";
+                } else {
+                    manager_string = manager.getInitials();
+                }
+                System.out.printf("ID: %s, title: %s, scheduled starting time: %s, manager: %s%n",
+                        matching.getProject_id(), matching.getTitle(), matching.getStart_date(),
+                        manager_string);
+            }
+        }
+    }
+
+    public void assignManager(String[] command_body) {
+        if (command_body.length != 3) {
+            System.out.println(syntax_error_message);
+        } else {
+            try {
+                int project_id = Integer.parseInt(command_body[1]);
+                String manager_initials = command_body[2];
+                Project project = this.planner_app.searchProjectsById(project_id);
+                Developer developer = this.planner_app.getDeveloper(manager_initials);
+                if (project == null || developer == null) {
+                    System.out.println("Manager or project could not be found. Please check your input");
+                } else {
+                    project.setProjectManager(developer);
+                    System.out.printf("Developer %s assigned as manager on project %s%n",
+                            developer.getInitials(), project.getTitle());
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Project ID was in incorrect format");
+            }
+        }
+    }
+
+    public void addDeveloper(String[] command_body) {
+        if (command_body.length != 2) {
+            System.out.println(syntax_error_message);
+        } else if (command_body[1].length() > 4) {
+            System.out.println("Developer name must be 4 characters at most");
+        }
+        else {
+            Developer developer = new Developer(command_body[1]);
+            try {
+                this.planner_app.addDeveloper(developer);
+                System.out.printf("Developer %s added%n", developer.getInitials());
+            } catch (OperationNotAllowedException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
@@ -119,6 +170,12 @@ public class UserInterface {
                     break;
                 case "search_id":
                     searchProject(user_input);
+                    break;
+                case "assign_manager":
+                    assignManager(user_input);
+                    break;
+                case "developer_add":
+                    addDeveloper(user_input);
                     break;
                 case "quit":
                 case "exit":
